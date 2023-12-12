4d230830594631d15235ec1286efb95e0401cf57
Rituraj <rajvermaritu15@gmail.com>
2023-12-12 18:56:55 +0530

    some css fix

--- medianblog/src/app/footer/Footer.jsx
+++ medianblog/src/app/footer/Footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
                         <div className="mb-6 md:mb-0">
                             <a href="#" className="flex items-center">
                                 {/* <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-8" alt="FlowBite Logo" /> */}
-                                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Median Blog</span>
+                                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">MedianBlog</span>
                             </a>
                         </div>
                         <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
@@ -20,7 +20,7 @@ function Footer() {
                                 <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
                                 <ul className="text-gray-600 dark:text-gray-400">
                                     <li className="mb-4">
-                                        <a href="#" className="hover:underline">Median Blog</a>
+                                        <a href="#" className="hover:underline">MedianBlog</a>
                                     </li>
                                     <li>
                                         <a href="https://tailwindcss.com/" className="hover:underline">Tailwind CSS</a>
@@ -33,7 +33,7 @@ function Footer() {
                                     <li className="mb-4">
                                         <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Linkedin</a>
                                     </li>
-                                    <li>
+                                    <li className='mb-4'>
                                         <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Instagram</a>
                                     </li>
                                     <li>
@@ -56,10 +56,14 @@ function Footer() {
                     </div>
                     <hr className="my-6 border-gray-400 sm:mx-auto dark:border-gray-200 lg:my-8" />
                     <div className="sm:flex sm:items-center sm:justify-between">
-                        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022
+                        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+                            © {
+                                (new Date()).getFullYear()+" "
+                            } 
                             <a href="#" className="hover:underline">MedianBlog™</a>. All Rights Reserved.
                         </span>
                         <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+                            {/* this is for placing e */}
                             <SocialMediaSVG/>
                         </div>
                     </div>
